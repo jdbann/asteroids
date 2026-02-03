69b5cc7bbfefb2a98db890821fbf8d12b817043c
John Bannister <johnb@hey.com>
2026-02-03 12:33:30 +0000

    Spawn asteroid on startup

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ use bevy::prelude::*;
 fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, (spawn_camera, spawn_asteroid))
         .run()
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+#[derive(Component)]
+struct Asteroid;
+
+fn spawn_asteroid(mut commands: Commands) {
+    commands.spawn(Asteroid);
 }
