e2821489cb30eceb6084697718462875d2d1c4fb
John Bannister <johnb@hey.com>
2026-02-06 14:00:51 +0000

    Spawn player and move with arrow keys

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() -> AppExit {
             PhysicsDebugPlugin,
         ))
         .insert_resource(Gravity::ZERO)
-        .add_systems(Startup, (spawn_camera, spawn_asteroid))
+        .add_systems(Startup, (spawn_camera, spawn_asteroid, spawn_player))
+        .add_systems(FixedUpdate, move_player)
         .run()
 }
 
@@ -31,11 +32,16 @@ fn spawn_asteroid(
 
     commands.spawn((
         Asteroid,
-        Transform::default(),
+        Transform::from_translation(vec3(
+            rng.random_range(-400.0..=400.0),
+            rng.random_range(-300.0..=300.0),
+            0.0,
+        )),
         RigidBody::Dynamic,
         Collider::convex_decomposition(vertices, indices),
-        LinearVelocity(vec2(20.0, 20.0)),
-        AngularVelocity(0.5),
+        LinearVelocity(rng.random::<Vec2>() * rng.random_range(-20.0..=20.0)),
+        AngularVelocity(rng.random::<f32>() * rng.random_range(-1.0..=1.0)),
+        Mass(2.0),
     ));
 }
 
@@ -54,4 +60,61 @@ fn generate_asteroid_geometry(rng: &mut bevy_prng::WyRand) -> (Vec<Vec2>, Vec<[u
     }
 
     (vertices, indices)
+}
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct ShipImpulses {
+    thrust: f32,
+    turn: f32,
+}
+
+fn spawn_player(mut commands: Commands) {
+    let (vertices, indices) = generate_player_geometry();
+
+    commands.spawn((
+        Player,
+        Transform::default(),
+        RigidBody::Dynamic,
+        Collider::convex_decomposition(vertices, indices),
+        Mass(1.0),
+        AngularDamping(5.0),
+        ShipImpulses {
+            thrust: 2.0,
+            turn: 20.0,
+        },
+    ));
+}
+
+fn generate_player_geometry() -> (Vec<Vec2>, Vec<[u32; 2]>) {
+    (
+        vec![
+            vec2(0.0, 15.0),
+            vec2(-10.0, -15.0),
+            vec2(0.0, -10.0),
+            vec2(10.0, -15.0),
+        ],
+        vec![[0, 1], [1, 2], [2, 3], [3, 0]],
+    )
+}
+
+fn move_player(
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&ShipImpulses, Forces), With<Player>>,
+) {
+    for (ship_impulses, mut forces) in &mut query {
+        if input.pressed(KeyCode::ArrowUp) {
+            forces.apply_local_linear_impulse(vec2(0.0, ship_impulses.thrust));
+        }
+
+        if input.pressed(KeyCode::ArrowLeft) {
+            forces.apply_angular_impulse(ship_impulses.turn);
+        }
+
+        if input.pressed(KeyCode::ArrowRight) {
+            forces.apply_angular_impulse(-1.0 * ship_impulses.turn);
+        }
+    }
 }
